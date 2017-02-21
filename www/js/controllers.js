1a9fe411e467bc00139bf249e999b05643c0d286
Antoine Sestier <antoine.sestier@campus-numerique-in-the-alp.com>
2017-02-21 08:54:35 +0100

    add search for chat list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,11 +10,27 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-
+/*  $scope.users = [
+    {name:'John', age:25, gender:'boy'},
+    {name:'Jessie', age:30, gender:'girl'},
+    {name:'Johanna', age:28, gender:'girl'},
+    {name:'Joy', age:15, gender:'girl'},
+    {name:'Mary', age:28, gender:'girl'},
+    {name:'Peter', age:95, gender:'boy'},
+    {name:'Sebastian', age:50, gender:'boy'},
+    {name:'Erika', age:27, gender:'girl'},
+    {name:'Patrick', age:40, gender:'boy'},
+    {name:'Samantha', age:60, gender:'girl'}
+  ];*/
+  
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
+
+  $scope.change = function(){
+    console.log("change");
+  }
 
 
 })
